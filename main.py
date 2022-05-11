e6113b731dc7cae1438d05d8cda6124545427ef8
Gustavo Martins <kernelgm@gmail.com>
2022-05-11 15:28:22 -0300

    work fine, except random_text

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import unidecode
+from random import randint
 from bs4 import BeautifulSoup
 
 
@@ -26,19 +27,34 @@ class Synonyms():
             self.synonyms.append(
                 data.get_text().strip().replace('.', '').split(','))
 
-        for index, word in enumerate(self.synonyms):
-            print(f'{index}, {word}')
-
     def word(self, word):
         self.word = unidecode.unidecode(word.strip().lower())
         self.url = f'http://www.sinonimos.com.br/{self.word}'
         self.resp_get = requests.get(self.url)
         self.soup = BeautifulSoup(self.resp_get.content, features='lxml')
+
         try:
             self.find_title()
             self.find_synonyms()
         except Exception as e:
             print(e)
+
+        for index, words in enumerate(self.synonyms):
+            print(f'{index}, {words}')
+
+    def random_word(self, word, accuracy=2):
+        self.word = unidecode.unidecode(word.strip().lower())
+        self.url = f'http://www.sinonimos.com.br/{self.word}'
+        self.resp_get = requests.get(self.url)
+        self.soup = BeautifulSoup(self.resp_get.content, features='lxml')
+        self.find_title()
+        self.find_synonyms()
+
+        self.lenght = len(self.synonyms[0])
+        self.random_synonyms = self.synonyms[0][randint(
+            0, int(self.lenght/accuracy))]
+
+        print(self.random_synonyms.strip())
 
     def text(self, text, size_of_word=5):
         self.full_text = text
@@ -67,9 +83,39 @@ Texto original: {self.full_text}
 Texto modificado: {self.final_text}
               ''')
 
+    def random_text(self, text, size_of_word=5):
+        self.full_text = text
+
+        self.partial_text = self.full_text.lower().strip()\
+            .replace('.', '').replace(',', '').split()
+
+        self.bag_of_words = [
+            word for word in self.partial_text if len(word) >= size_of_word
+        ]
+
+        for single_word in self.bag_of_words:
+            print(f'\nTexto: {" ".join(self.partial_text)}\n')
+
+            Synonyms().random_word(single_word)
+
+            for item in range(len(self.partial_text)):
+                if self.partial_text[item] == single_word:
+                    self.partial_text[item] = self.random_word(single_word)
+
+            self.final_text = ' '.join(self.partial_text)
+
+        print(f'''
+Texto original: {self.full_text}
+Texto modificado: {self.final_text}
+              ''')
+
 
 if __name__ == '__main__':
-    texto = 'Para começar o projeto você pode optar por várias técnicas, \
-a fim de coletar as informações necessárias'
+    texto = 'Para começar o projeto você pode optar por várias técnicas, a fim de coletar as informações necessárias'
 
-    Synonyms().text(texto)
+    palavra = 'precisão'
+
+    # Synonyms().word(palavra)
+    # Synonyms().text(texto)
+    # Synonyms().random_word(palavra)
+    Synonyms().random_text(texto)
