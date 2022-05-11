11741b2348e5db45e5e7f5b2c02bdb38d4714a67
Gustavo Martins <kernelgm@gmail.com>
2022-05-11 13:42:13 -0300

    Change all for classe main

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
-import string
-import unidecode
 import requests
+import unidecode
 from bs4 import BeautifulSoup
 
 
 class Synonyms():
-    def __init__(self, word) -> None:
-        """Inicia o processo de busca na pagina sinonimos.com.br.
-
-        Args:
-            word (str): palavra a ser procurada no site
-        """
-        self.word = unidecode.unidecode(word.strip().lower())
-        self.url = f'http://www.sinonimos.com.br/{self.word}'
-        self.resp_get = requests.get(self.url)
-        self.soup = BeautifulSoup(self.resp_get.content, features='lxml')
+    def __init__(self) -> None:
         self.synonyms = []
-        self.replace = []
-        self.string = string
 
     def find_title(self):
-        """Acha o titulo, algo como: sinonimo de "palavra" e printa isso
-        se não achar é porque a palavra foi digitada errada ou não tem
-        no banco de dados.
-        """
         try:
             print(self.soup.find('h1', {'class': 'h-palavra'}).get_text())
         except Exception:
-            print('Essa palavra não existe no banco de dados ou você a digitou \
-errado!')
+            print('Essa palavra não existe no BD ou você a digitou errado!')
 
     def find_synonyms(self):
-        """Acha os sinonimos da palavra desejada ordenando cada uma com
-        o sentido, a ordem e as palavras equivalentes.
-        """
         for data in self.soup.find_all('p', {'class': 'sinonimos'}):
             for remove_example in self.soup.find_all('span'):
                 for remove_number in self.soup.find_all('em'):
@@ -49,31 +29,35 @@ errado!')
         for index, word in enumerate(self.synonyms):
             print(index, word)
 
-    def run_for_word(self):
-        """Executa todos os métodos anteriores.
-        """
+    def word(self, word):
+        self.word = unidecode.unidecode(word.strip().lower())
+        self.url = f'http://www.sinonimos.com.br/{self.word}'
+        self.resp_get = requests.get(self.url)
+        self.soup = BeautifulSoup(self.resp_get.content, features='lxml')
         try:
-            Synonyms(self.word)
             self.find_title()
             self.find_synonyms()
-            print('\n')
         except Exception as e:
             print(e)
 
-    def run_for_text(self, string):
-        string.lower().strip().replace('.', '').replace(',', '').split()
-        bag_of_words = [word for word in string if len(word) >= 5]
+    def text(self, text, size_of_word=5):
+        self.text = text.lower().strip()\
+            .replace('.', '').replace(',', '').split()
 
-        for word in bag_of_words:
-            Synonyms(word).run_for_word()
-            self.replacereplace.append(
-                input(f'Por qual palavra quer substituir {word}? '))
+        self.bag_of_words = [
+            word for word in self.text if len(word) >= size_of_word
+        ]
 
-        print(self.replace)
+        for single_word in self.bag_of_words:
+            Synonyms().word(single_word)
 
 
 if __name__ == '__main__':
-    string = 'Para começar o projeto você pode optar por várias técnicas, \
+    texto = 'Para começar o projeto você pode optar por várias técnicas, \
         a fim de coletar as informações necessárias'
 
-    Synonyms().run_for_text(string=string)
+    Synonyms().text(texto)
+
+    palavra = 'achar'
+
+    Synonyms().word(palavra)
